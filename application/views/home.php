bb2180b94a268487d17c971c2d3c814662695c82
rifqinafis <43541017+rifqinafis@users.noreply.github.com>
2021-04-04 18:15:02 +0700

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -4,7 +4,7 @@
     <!-- Main component for a primary marketing message or call to action -->
     <div class="jumbotron" style="text-align: center; ">
         <h2>SISTEM PENDUKUNG KEPUTUSAN</h2>
-        <img style="height: 250px; width: 450px; margin-top: 20px" src="<?php echo base_url() ?>/Asset/images/016255100_1487375130-Martial-arts-21.jpg">
+        <img style="height: 250px; width: 450px; margin-top: 20px" src="<?php echo base_url() ?>/Asset/images/buruh-pekerja-pegawai-karyawan-profesional.png">
         <h2 style="line-height: 150%;">SELEKSI CALON KARYAWAN MENGGUNAKAN METODE MATCHING PROFILE DAN ANALYTICAL HIERARCHY PROCESS</h2>
     </div>
 </div>
